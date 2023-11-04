65a910b46ce590b443d102f9d73252f927013e3a
pressdot <anu1@163.com>
2023-11-04 15:22:45 +0800

    Update IdleMemory.c

--- configs/FreeRTOS-Kernel/IdleMemory.c
+++ configs/FreeRTOS-Kernel/IdleMemory.c
@@ -4,7 +4,7 @@
 /* configSUPPORT_STATIC_ALLOCATION is set to 1, so the application must provide an
 implementation of vApplicationGetIdleTaskMemory() to provide the memory that is
 used by the Idle task. */
-//Adjusted to the latest FreeRTOS
+//Adjusted to the latest FreeRTOS Release
 void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
                                     StackType_t **ppxIdleTaskStackBuffer,
                                     uint32_t *pulIdleTaskStackSize,BaseType_t xCoreId )
@@ -12,20 +12,11 @@ void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
 /* If the buffers to be provided to the Idle task are declared inside this
 function then they must be declared static – otherwise they will be allocated on
 the stack and so not exists after this function exits. */
-static StaticTask_t xIdleTaskTCB;
-static StackType_t uxIdleTaskStack[ configMINIMAL_STACK_SIZE ];
-
-    /* Pass out a pointer to the StaticTask_t structure in which the Idle task’s
-    state will be stored. */
-    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-    /* Pass out the array that will be used as the Idle task’s stack. */
-    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-    Note that, as the array is necessarily of type StackType_t,
-    configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  	static StaticTask_t xIdleTaskTCBs[ configNUMBER_OF_CORES ];
+	static StackType_t uxIdleTaskStacks[ configNUMBER_OF_CORES ][ configMINIMAL_STACK_SIZE ];
+	*ppxIdleTaskTCBBuffer = &( xIdleTaskTCBs[ xCoreId ] );
+	*ppxIdleTaskStackBuffer = &( uxIdleTaskStacks[ xCoreId ][ 0 ] );
+	*pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
 }
 /* configSUPPORT_STATIC_ALLOCATION and configUSE_TIMERS are both set to 1, so the
 application must provide an implementation of vApplicationGetTimerTaskMemory()
